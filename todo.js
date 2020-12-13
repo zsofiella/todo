c06601bbc7d350ce08f8122b3763e86238ae4b29
Zsofi <galambvarizsofia@gmail.com>
2020-12-13 19:52:46 +0100

    js finished according to video

--- todo.js
+++ todo.js
@@ -25,9 +25,70 @@
             if (!value) {
                 return null;
             }
-            return JSON
+            return JSON.parse(value);
+        };
+        removeItem(key){
+            localStorage.removeItem(key);
+        };
+
+        const init = () => {
+            showDate();
+            setListeners();
+            loadExistingTodos();
+
+        };
+        
+        const loadExistingTodos = () => {
+            const savedTodos = localDB.getItem('todos');
+            if (savedTodos){
+                todos = savedTodos;
+            }  
+            if (todos && Array.isArray(todos)) {
+                todos.forEach( todo => showTodo(todo));
+            }      
+        };
+        
+        const showDate = () => {
+            const currentDate = New Date();
+            const day = [
+                currentDate.getMonth() + 1,
+                currentDate.getDate(),
+                currentDate.getFullYear(),
+            ].map( num => num < 10 ? `0${num}` : num );
+            bodyDay.textContent = dayNames[currentDate.getDay()];
+            bodyDate.textContent = day.join('-');
+        };
+
+        const setListeners = () => {
+            todoAddBtn.addEventListener('click', addNewTodo);
+        };
+        const addNewTodo = () => {
+            const value = todoInput.value;
+            if (value === ''){
+                alert('Please type a todo.');
+                return;
             }
-        }
-        }
-    }
-})
+            const todo = {
+                text: value,
+                done: false
+            };
+            todos.push(todo);
+            localDB.setItem('todos', todos);
+            showTodo(todo);
+            todoInput.value = '';
+        };
+
+        const showTodo = todo => {
+            const todoItem = document.createElement('.div');
+            todoListPending.appendChild(todoItem);
+
+            todoItem.innerHTML = `
+            <input type="checkbox">
+            <span>${todo.text}</span>
+            <button>
+                <i class="fa fa-trash"></i>
+            </button>
+            `;
+        };
+        init();
+    })();
